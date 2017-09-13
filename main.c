4301c66926069e122ae7da2e56ff6d20a7ee5986
ljy325732 <31815289+ljy325732@users.noreply.github.com>
2017-09-13 22:39:47 +0900

    Update and rename adsf.c to main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+//orignal libraries
+#include <stdio.h>//For standard I/O Operation
+#include <stdlib.h>//To use functions like system(); system("dos command"); - this performs the dos command given.
+                   // Eg. system("del abc.txt"); - This command deletes the file abc.txt from current location of project
+#include <windows.h>//Used in making gotoxy(int, int) function
+#include <conio.h>//For getch(); getche() type of functions
+#include <time.h>//To get system date and time
+#include <math.h>//To calculate mathematical problems, used esp. in interest calculation
+
+//optional header
+//다같이
+#include"data.h"//life, score, items, save/load files
+
+//Minie Park
+#include"graphics.h"//board, ui, ... img files
+#include"dot.h"//if player eat dot ::= score up
+#include"player.h"//cases : player touch dot or ghost
+
+//Juyeong Yeon
+#include"ghost.h"//ghost
+
+int main(void){
+     
+}
